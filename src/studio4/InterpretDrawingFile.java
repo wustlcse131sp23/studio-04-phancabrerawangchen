0bcf6410acb0bc6e60e57c9ac259a0ca7ca0b956
giuli <giuli@DESKTOP-B5F4QNE.dhcp.wustl.edu>
2023-02-23 12:26:18 -0600

    Finished flag and reading file

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,26 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+	
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.5, .4, .2 );
+		
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.filledRectangle(0.8, 0.5, .05, .05 );
+		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.2);
+		
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.filledRectangle(0.2, 0.4, .1, .1 );
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledEllipse(0.75, 0.6, 0.1, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.line(0.2, 0.3, 0.4, 0.5);
+		StdDraw.line(0.2, 0.3, 0.6, 0.3);
+		StdDraw.line(0.6,0.3,0.4,0.5);
 	}
 }
